83671e540771044449c23d15788975dbcc223c1c
Eric <bonderic44@gmail.com>
2024-01-01 02:27:21 +0545

    feat : added chocolatey autocompletion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -383,3 +383,11 @@ function Get-WhoIs {
 }
 
 New-Alias -Name whois -Value Get-WhoIs
+
+# Import the Chocolatey Profile that contains the necessary code to enable tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
